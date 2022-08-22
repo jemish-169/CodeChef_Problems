851892a2d57e1d3c0f06cad5e51fca4491e56320
jemish_169 <jemishkhunt169@.com>
2022-08-22 22:23:17 +0530

    22nd aug contest

---
+++ HeightofRationals.cpp
@@ -0,0 +1,24 @@
+/*
+Height of Rationals
+*/
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+set<int> st;
+map<int, int> mp;
+list<int> ls;
+vector<int> vec;
+int maxi = INT_MIN;
+int mini = INT_MAX;
+int32_t main()
+{
+    int cases = 1;
+    // cin >> cases;
+    while (cases--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << max(a, b);
+    }
+    return 0;
+}
