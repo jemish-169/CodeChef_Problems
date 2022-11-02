28673448a22835f5c614924cce3f5dbce1826749
jemish-169 <jemishkhunt169@gmail.com>
2022-11-02 23:37:31 +0530

    bh

---
+++ OddSumPair.cpp
@@ -0,0 +1,34 @@
+/*
+Odd Sum Pair
+*/
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+set<int> st;
+map<int, int> mp;
+list<int> ls;
+vector<int> vec;
+int maxi = INT_MIN;
+int mini = INT_MAX;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int cases = 1;
+    cin >> cases;
+    while (cases--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if ((a + b) % 2 == 1)
+            cout << "YES" << endl;
+        else if ((c + b) % 2 == 1)
+            cout << "YES" << endl;
+        else if ((c + a) % 2 == 1)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
