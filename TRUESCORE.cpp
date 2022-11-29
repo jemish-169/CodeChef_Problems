ab2f3d4a4c3a263101b5546de4cd5473c3047bc2
jemish-169 <jemishkhunt169@gmail.com>
2022-11-29 23:59:29 +0530

    no message

--- TRUESCORE.cpp
+++ TRUESCORE.cpp
@@ -1,19 +1,7 @@
-/*
-Is the score consistent
-*/
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int main()
-{
-    int cases, a, b, c, d;
-    cin >> cases;
-    while (cases--)
-    {
-        cin >> a >> b >> c >> d;
-        if (b <= d && c >= a)
-            cout << "POSSIBLE" << endl;
-        else
-            cout << "IMPOSSIBLE" << endl;
-    }
-    return 0;
+
+int main() {
+	// your code goes here
+	return x;
 }
