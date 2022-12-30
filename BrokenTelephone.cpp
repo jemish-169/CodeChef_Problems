e969e0321b3cc9f0a49a9d5cddef2ba5c572dbce
jemish-169 <jemishkhunt169@gmail.com>
2022-12-30 23:31:31 +0530

    no message

---
+++ BrokenTelephone.cpp
@@ -0,0 +1,34 @@
+/*
+Broken Telephone
+*/
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+set<int> st;
+map<int, int> mp;
+list<int> ls;
+int maxi = INT_MIN;
+int mini = INT_MAX;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int cases = 1;
+    cin >> cases;
+    while (cases--)
+    {
+        int x, ans = 0;
+        cin >> x;
+        vector<int> vec(x);
+        for (int i = 0; i < x; i++)
+            cin >> vec[i];
+        for (int i = 1; i < x; i++)
+        {
+            if (vec[i] != vec[i - 1])
+                ans++;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
