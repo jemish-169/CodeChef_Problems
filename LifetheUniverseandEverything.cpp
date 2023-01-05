4ce37d3b8721c3d47baa54b7a71adb15e002f72d
jemish-169 <jemishkhunt169@gmail.com>
2023-01-05 22:29:18 +0530

    no message

---
+++ LifetheUniverseandEverything.cpp
@@ -0,0 +1,29 @@
+/*
+Life, the Universe, and Everything
+*/
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+set<int> st;
+map<int, int> mp;
+list<int> ls;
+vector<int> vec;
+int maxi = INT_MIN;
+int mini = INT_MAX;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int cases = 1;
+    // cin >> cases;
+    while (true)
+    {
+        int x;
+        cin >> x;
+        if (x == 42)
+            return 0;
+        cout << x << endl;
+    }
+    return 0;
+}
