9383f6868f31fa16579f7b7a10aa4f2d3a4c7663
jemish-169 <jemishkhunt169@gmail.com>
2023-03-21 23:56:46 +0530

    no message

--- JustOneMoreEpisode.cpp
+++ JustOneMoreEpisode.cpp
@@ -4,10 +4,7 @@ Just One More Episode
 #include <bits/stdc++.h>
 #define int int64_t
 using namespace std;
-set<int> st;
-map<int, int> mp;
-list<int> ls;
-vector<int> vec;
+
 int maxi = INT_MIN;
 int mini = INT_MAX;
 int32_t main()
